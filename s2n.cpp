9ed2df094f46e7d60108b01535e26dc7681c4ffd
root <--global>
2014-06-03 18:44:56 +0100

    Oops.

--- s2n.cpp
+++ s2n.cpp
@@ -10,8 +10,7 @@
 #include "Prerequisites.h"
 #include "S2Daemon.h"
 
-#define ENABLE_DAEMON
-
+#ifdef ENABLE_DAEMON
 typedef ssize_t(*tsendto)(int,const void*,size_t,int,const struct sockaddr*,socklen_t);
 ssize_t sendto(int sockfd, const void* buf, size_t len, int flags, const struct sockaddr* dest_addr, socklen_t addrlen)
 {
